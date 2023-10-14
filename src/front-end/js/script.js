748816c2d738ebe6f6904f8b60ea2f94dfa1acb8
Alison Reges <alisonreges@gmail.com>
2023-10-14 13:12:11 -0300

    Atualização do front

--- src/front-end/js/script.js
+++ src/front-end/js/script.js
@@ -2,70 +2,70 @@ var navBar = document.getElementById('navBar');
 var clique = false;
 
 function toggleNavBar() {
-  if (clique) {
-    navBar.style = ' background: none; height: auto; min-width: 0; border-radius: 0; ';
-    listaUL.style = 'display: none; ';
-    clique = false;
-    navBar.classList.remove('active');
-  } else {
-    navBar.style = 'background: #261B1D; height: 50vh; min-width: 5ch; border-radius: 12px; border-top-right-radius: 0; border-top-left-radius: 0;';
-    listaUL.style = 'display: flex; ';
-    clique = true;
-    navBar.classList.add('active');
-  }
+    if (clique) {
+        navBar.style = ' background: none; height: auto; min-width: 0; border-radius: 0; ';
+        listaUL.style = 'display: none; ';
+        clique = false;
+        navBar.classList.remove('active');
+    } else {
+        navBar.style = 'background: #261B1D; height: 50vh; min-width: 5ch; border-radius: 12px; border-top-right-radius: 0; border-top-left-radius: 0;';
+        listaUL.style = 'display: flex; ';
+        clique = true;
+        navBar.classList.add('active');
+    }
 }
 
 navBar.addEventListener('click', () => {
-  if (window.innerWidth < 450) {
-    toggleNavBar();
-  }
+    if (window.innerWidth < 450) {
+        toggleNavBar();
+    }
 });
 
 window.addEventListener('resize', () => {
-  if (window.innerWidth >= 450) {
-    navBar.style = '';
-    listaUL.style = '';
-    clique = false;
-    navBar.classList.remove('active');
+    if (window.innerWidth >= 450) {
+        navBar.style = '';
+        listaUL.style = '';
+        clique = false;
+        navBar.classList.remove('active');
 
-  }
+    }
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const menuItems = document.querySelectorAll(".Lista");
+document.addEventListener("DOMContentLoaded", function() {
+    const menuItems = document.querySelectorAll(".Lista");
 
-  menuItems.forEach(function (menuItem) {
-    const submenu = menuItem.querySelector(".submenu");
-    if (submenu) {
-      submenu.style.display = "none"; // Inicialmente, oculta o submenu
+    menuItems.forEach(function(menuItem) {
+        const submenu = menuItem.querySelector(".submenu");
+        if (submenu) {
+            submenu.style.display = "none"; // Inicialmente, oculta o submenu
 
-      menuItem.addEventListener("click", function (event) {
-        event.preventDefault();
-        submenu.style.display = submenu.style.display === "block" ? "none" : "block";
-      });
+            menuItem.addEventListener("click", function(event) {
+                event.preventDefault();
+                submenu.style.display = submenu.style.display === "block" ? "none" : "block";
+            });
 
-      // Adicione um ouvinte de eventos para fechar o submenu quando se clica em qualquer outro item do menu
-      const otherMenuItems = document.querySelectorAll(".Lista:not(#" + menuItem.id + ")");
-      otherMenuItems.forEach(function (otherMenuItem) {
-        otherMenuItem.addEventListener("click", function (event) {
-          submenu.style.display = "none";
-        });
-      });
+            // Adicione um ouvinte de eventos para fechar o submenu quando se clica em qualquer outro item do menu
+            const otherMenuItems = document.querySelectorAll(".Lista:not(#" + menuItem.id + ")");
+            otherMenuItems.forEach(function(otherMenuItem) {
+                otherMenuItem.addEventListener("click", function(event) {
+                    submenu.style.display = "none";
+                });
+            });
 
-      // Adicionar um evento de clique aos itens do submenu
-      const submenuItems = submenu.querySelectorAll("li");
-      submenuItems.forEach(function (submenuItem) {
-        submenuItem.addEventListener("click", function (event) {
-          // Evita que o evento de clique no submenu se propague para o menu principal e o feche
-          event.stopPropagation();
+            // Adicionar um evento de clique aos itens do submenu
+            const submenuItems = submenu.querySelectorAll("li");
+            submenuItems.forEach(function(submenuItem) {
+                submenuItem.addEventListener("click", function(event) {
+                    // Evita que o evento de clique no submenu se propague para o menu principal e o feche
+                    event.stopPropagation();
 
-          // Aqui você pode adicionar o código para o que deseja fazer quando um item do submenu é clicado
-          // Por exemplo, redirecionar para outra página ou executar uma ação específica
-          console.log("Clicou em um item do submenu:", submenuItem.textContent);
-        });
-      });
-    }
-  });
+                    // Aqui você pode adicionar o código para o que deseja fazer quando um item do submenu é clicado
+                    // Por exemplo, redirecionar para outra página ou executar uma ação específica
+                    console.log("Clicou em um item do submenu:", submenuItem.textContent);
+                });
+            });
+        }
+    });
 });
 //======================= MODAL =============================================
 
@@ -76,33 +76,36 @@ const pessoa = document.querySelector("#pessoas");
 const home = document.querySelector("#home");
 
 home.addEventListener("click", async () => {
-  const tela = await loadHTML("main", "home.html")
+    const tela = await loadHTML("main", "home.html")
 })
 
 
 pessoa.addEventListener("click", async () => {
-  const tela = await loadHTML("main", "pessoas.html");
+    const tela = await loadHTML("main", "pessoas.html");
 })
 
 
 function loadHTML(id, filename) {
-  let xhttp;
-  let element = document.getElementById(id)
-    ;
-  let file = filename;
+    let xhttp;
+    let element = document.getElementById(id);
+    let file = filename;
 
-  if (file) {
+    if (file) {
 
-    xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4) {
-        if (this.status == 200) { element.innerHTML = this.responseText; }
-        if (this.status == 404) { element.innerHTML = "<h1>Page not found.</h1>"; }
-      }
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    element.innerHTML = this.responseText;
+                }
+                if (this.status == 404) {
+                    element.innerHTML = "<h1>Page not found.</h1>";
+                }
+            }
+        }
+        xhttp.open("GET", `paginas/${file}`, true);
+        xhttp.send();
+        return xhttp;
     }
-    xhttp.open("GET", `paginas/${file}`, true);
-    xhttp.send();
-    return xhttp;
-  }
 
 }
